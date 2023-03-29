eae383d8b3f0ed86d7fa60ca0fe883865690bd8d
suriya096 <124135327+suriya096@users.noreply.github.com>
2023-03-29 15:53:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(){
+    let x = Number(document.getElementById("submit1").value);
+    let y = Number(document.getElementById("submit2").value);
+    let add  =x+y;
+    alert(add);
+}
+function sub(){
+    let x = Number(document.getElementById("submit1").value);
+    let y = Number(document.getElementById("submit2").value);
+    let sub  =x-y;
+    alert(sub);
+}
+function mult(){
+    let x = Number(document.getElementById("submit1").value);
+    let y = Number(document.getElementById("submit2").value);
+    let mult  =x*y;
+    alert(mult);
+}
+function div(){
+    let x = Number(document.getElementById("submit1").value);
+    let y = Number(document.getElementById("submit2").value);
+    let div  =x/y;
+    alert(div);
+}
+function mod(){
+    let x = Number(document.getElementById("submit1").value);
+    let y = Number(document.getElementById("submit2").value);
+    let mod  =x%y;
+    alert(mod);
+}
